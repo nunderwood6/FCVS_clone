6852052fd82069e9266c67f641024d14c15753cf
Nick Underwood <nunderwood6@gmail.com>
2018-06-22 12:19:45 -0500

    Check if radar image loads

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ function createMap(){
 
 
     //add base tilelayer
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
       	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">Carto</a>',
       	subdomains: 'abcd'
     }).addTo(map);
